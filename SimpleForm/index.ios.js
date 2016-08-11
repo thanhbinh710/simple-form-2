0357386bcd5a94c46d2fe91c7b10da99db6c6a3f
thanhbinh710 <thanhbinh710@gmail.com>
2016-08-11 19:56:50 +0800

    Basic layout without styling for iOS app
    Rendering different content into  different tab for SegmentedControl
    depends on the index selected

--- SimpleForm/index.ios.js
+++ SimpleForm/index.ios.js
@@ -1,53 +1,138 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+'use strict';
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  TextInput,
+  View,
+  ListView,
+  SegmentedControlIOS
 } from 'react-native';
 
-class SimpleForm extends Component {
+import Button from 'apsl-react-native-button';
+
+var SimpleForm = React.createClass({
+  render() {
+   return(
+    <View style={{flex: 1, marginTop:30}}>
+      <View style={{flex: 1, backgroundColor: '#00B140'}} />
+      <View style={{flex: 10 }}>
+        <ProgressBar />
+      </View>
+    </View>
+
+
+   )
+  }
+});
+
+var ProgressBar = React.createClass({
+  getInitialState() {
+    return {
+      values: ['Step 1', 'Step 2', 'Step 3'],
+      selectedTab: 'Step 1',
+      selectedIndex: 0,
+      text: '',
+    };
+  },
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      <View style={{flex: 1}}>
+        <View style={{flex: 0.8, backgroundColor: '#fff'}}>
+          <SegmentedControlIOS
+            style={{flex: 1 }}
+            values={this.state.values}
+            selectedIndex={0}
+
+            onValueChange={(val) => {
+              this.setState({
+                selectedTab: val
+              })
+            }} />
+        </View>
+
+        <View style={{flex: 9.2, backgroundColor: '#F7F9FB'}}>
+          {this.renderStepView()}
+        </View>
+
       </View>
     );
-  }
-}
+  },
 
-const styles = StyleSheet.create({
+  renderStepView: function() {
+
+    if (this.state.selectedTab === 'Step 1') {
+      return (
+        <View style={{flex: 1}}>
+
+          <Text>Upload Photo</Text>
+          <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+          <Text>Please upload any photo here.</Text>
+          <Button style={{backgroundColor: '#00B140'}}>
+            NEXT
+          </Button>
+        </View>
+        )
+    } else if (this.state.selectedTab === 'Step 2') {
+      return (
+        <View style={{flex: 1}}>
+
+          <Text>Your Contact Information</Text>
+          <Text>Fill in the following details</Text>
+
+          <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+            placeholder="Your First Name"
+            onChangeText={(text) => this.setState({text})}
+          />
+
+          <Button style={{backgroundColor: '#00B140'}}>
+            NEXT
+          </Button>
+
+        </View>
+        )
+    } else {
+      return (
+        <View style={{flex: 1}}>
+          <Text>Complete.</Text>
+        </View>
+        )
+    }
+  },
+
+});
+
+var styles = StyleSheet.create({
+  text: {
+    fontSize: 14,
+    textAlign: 'center',
+    fontWeight: '500',
+    margin: 10,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F7F9FB',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
 });
 
+
+exports.title = '<SimpleForm>';
+exports.displayName = 'SimpleForm';
+exports.examples = [
+  {
+    title: 'Change events can be detected',
+    render(): ReactElement { return <ProgressBar />; }
+  }
+];
+
+
+
 AppRegistry.registerComponent('SimpleForm', () => SimpleForm);
+
+module.exports = SimpleForm;
